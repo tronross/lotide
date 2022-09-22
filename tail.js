96e530260f4fe4d50e6deacb6d8be2602625cefb
John Ross <john_ross@analogforever.ca>
2022-09-22 18:06:28 -0400

    tail.js returns a shallow copy of the tail of the array.

---
+++ tail.js
@@ -0,0 +1,20 @@
+// Assert Equal testing code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Return a shallow copy of the tail of the array.
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(words);
+console.log(tail(words));
