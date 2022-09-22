4f86770f3e34d09098a7cd5fa980130fe8d4fabf
John Ross <john_ross@analogforever.ca>
2022-09-22 11:16:02 -0400

    Colourful console assert function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(75, 956);
+assertEqual("Barlo is sooo hungry", "Barlo is sooo hungry");
+assertEqual(99, "Ice Cream");
