0233a19567ea22b6af8eb836574c2182f549b87b
John Ross <john_ross@analogforever.ca>
2022-09-24 15:56:16 -0400

    First draft of without.js without implemented test conditions.

---
+++ without.js
@@ -0,0 +1,47 @@
+const without = function(source, itemsToRemove) {
+  let editedArray = [];
+
+  for (element of source) {
+    for (i = 0; i < itemsToRemove.length; i++) {
+      if (element !== itemsToRemove[i]) {
+        editedArray.push(element);
+      }
+    }
+  }
+  return editedArray;
+}
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let arraysEqual = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    arraysEqual = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        arraysEqual = false;
+      }
+    }
+  }
+
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+    return equal;
+  }
+};
