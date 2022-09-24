da02f93c0d57313c22b575aae1ef95d96bb75c4f
John Ross <john_ross@analogforever.ca>
2022-09-24 12:44:47 -0400

    A function to check if arrays are identical, also utilizes assertEqual.

---
+++ eqArrays.js
@@ -0,0 +1,36 @@
+// Assert Equal testing code
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// A function to check if arrays are identical
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+      return equal;
+  }
+};
+
+
+// Test cases utilizing assertEqual:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);// => fail
+assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true);// => fail
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => fail
+assertEqual(eqArrays(["banana", "canteloupe", "watermelon"], ["banana", "canteloupe", "watermelon"]), true); // => pass
+assertEqual(eqArrays(["banana", "canteloupe", "watermelon"], ["banana", "antelope", "watermelon"]), true); // => fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => fail
+assertEqual(eqArrays([true, true],[true, false]), true); // => fail
+assertEqual(eqArrays([true, true],[true, true]), true); // => true
