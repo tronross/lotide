4f7c2a7aa13da8449c497449ebfe7363a944fba8
John Ross <john_ross@analogforever.ca>
2022-09-25 21:10:01 -0400

    Cleaned up file to increase readability.

--- flatten.js
+++ flatten.js
@@ -1,5 +1,30 @@
-// A function which takes in arrays containing elements icluding nested arrays of elements, and returns a "flattened" version of the array.
+// TEST/ASSERTION FUNCTIONS
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const arraysEqual = eqArrays(arrayOne, arrayTwo);
+ 
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
 
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+    return equal;
+  }
+};
+
+// FLATTEN: A function which takes in arrays containing elements including nested arrays of elements, and returns a "flattened" version of the array.
 const flatten = function(array) {
   let flattenedArray = [];
 
@@ -15,7 +40,7 @@ const flatten = function(array) {
   return flattenedArray;
 };
 
-// Test Conditions:
+// Test Code:
 console.log([1, 2, [3, 4], 5, [6]]);
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, [3, 4], 5, [6]]);
@@ -25,41 +50,6 @@ console.log(flatten(["1", "2", "3", ["11", "12"]]));
 assertArraysEqual((flatten(["1", "2", "3", ["11", "12"]])), ["1", "2", "3", ["11", "12"]]);
 
 // TEST CASE TO BE REVIEWED WITH MENTOR: WILL NOT RECOGNIZE NESTED ARRAY WITH TEXT STRING
-//console.log(flatten(['yes', ['no'], 'maybe']));
-
-// A function to compare two arrays and assert if they are equal.
-function assertArraysEqual(arrayOne, arrayTwo) {
-  let arraysEqual = true;
-
-  if (arrayOne.length !== arrayTwo.length) {
-    arraysEqual = false;
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        arraysEqual = false;
-      }
-    }
-  }
-
-  if (arraysEqual) {
-    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
-  }
-}
-
-// A function to check if arrays are identical
-const eqArrays = function(arrayOne, arrayTwo) {
-  let equal = true;
-
-  if (arrayOne.length !== arrayTwo.length) {
-    equal = false;
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        equal = false;
-      }
-    }
-    return equal;
-  }
-};
+console.log(['yes', ['no'], 'maybe']);
+console.log(flatten(['yes', ['no'], 'maybe']));
+assertArraysEqual((flatten(['yes', ['no'], 'maybe'])), ['yes', ['no'], 'maybe']);
