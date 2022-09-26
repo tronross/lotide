cf7029e48473cb920897e6bec8bdd1466bcd5fa7
John Ross <john_ross@analogforever.ca>
2022-09-26 19:29:31 -0400

    Declared countOnly with arguments, and pasted in assertEqual as assertion code.

---
+++ countOnly.js
@@ -0,0 +1,18 @@
+// TEST/ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of strings to be searched
+
+// itemsToCount: an object specifying what to count
+
+const countOnly = function(allItems, itemsToCount) {
+
+}
+
+
