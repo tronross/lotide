5aa2be65b5c1b9e694f00e5718f202f8ee09f387
John Ross <john_ross@analogforever.ca>
2022-09-25 21:20:35 -0400

    Imported assertion functions. Declared middle function and return variable, began conditional logic.

---
+++ middle.js
@@ -0,0 +1,36 @@
+// TEST/ASSERTION FUNCTIONS
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const arraysEqual = eqArrays(arrayOne, arrayTwo);
+ 
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+    return equal;
+  }
+};
+
+// middle:
+const middle = function(array) {
+  let middleArray = []
+  
+  if (array.length <= 2) {
+    return middleArray;
+  } else if {
+    
+  }
+}
