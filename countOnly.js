af60f7f21a1203730ff9cabdc8040ac29b613443
John Ross <john_ross@analogforever.ca>
2022-09-26 22:24:48 -0400

    Added conditional logic to only count the item if allItems[item] returns true. Moved return statement to correct scope to return object correctly.

--- countOnly.js
+++ countOnly.js
@@ -15,16 +15,17 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {};
   
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    }
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     console.log(item);
+    }
   }
-  
   return results;
-}
+};
 
 
 // TEST CODE:
