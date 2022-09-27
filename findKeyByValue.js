35645d86554266d690296b2f1cb3efb3bb694861
John Ross <john_ross@analogforever.ca>
2022-09-27 19:37:28 -0400

    Declared function findKeyByValue; pasted in assertion code and initial test code.

---
+++ findKeyByValue.js
@@ -0,0 +1,35 @@
+// ASSERTION CODE:
+// A function to check if arrays are identical
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+    return equal;
+  }
+};
+
+
+//FUNCTION
+
+const findKeyByValue = function(searchValue, objToSearch) {
+
+
+}
+
+
+// TEST CODE:
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
