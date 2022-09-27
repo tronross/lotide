feaf88626a04a9669992d98d8782f35ad9f43c58
John Ross <john_ross@analogforever.ca>
2022-09-27 17:54:00 -0400

    Declared function letterPositions; comments describing function; pasted in assertion code.

---
+++ letterPositions.js
@@ -0,0 +1,38 @@
+// A function to compare two arrays and assert if they are equal.
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const arraysEqual = eqArrays(arrayOne, arrayTwo);
+ 
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
+
+
+// A function to check if arrays are identical
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+    return equal;
+  }
+};
+
+
+// letterPositions: Returns an object containing keys that correspond to the individual letters in the input (argument) and values that are arrays containing the indices of where in the string the letters appeared.
+
+const letterPositions = function(sentence) {
+  const results = {};
+
+
+
+  return results;
+}
