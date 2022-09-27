6ba342f605a2aaa196fbbce3b549e8655cf7d675
tronross <john_ross@analogforever.ca>
2022-09-27 15:44:42 -0400

    Declared function 'countLetters' and incorporated eqArray as assertion code.

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+// TEST/ASSERTION FUNCTION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// countLetters: Take in a sentence (as a string) and return the count of each letter from that sentence.
+const countLetters = function(sentence) {
+
+};
