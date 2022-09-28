944e0b8f13a43d9676721be3605cde907362ba9e
John Ross <john_ross@analogforever.ca>
2022-09-27 20:54:38 -0400

    Successfully using nested for in loops to check key-value pairs, and logging the keys and values to the console for testing.

--- eqObjects.js
+++ eqObjects.js
@@ -10,6 +10,16 @@ const assertEqual = function(actual, expected) {
 // FUNCTION
 // eqObjects: Takes in two objects and returns true on a purrfekt match, and false on anything else.
 const eqObjects = function(object1, object2) {
+  for (const key in object1) {
+    console.log(key);
+    for (const key2 in object2) {
+      if (key === key2) {
+        console.log(object1[(key)], object2[(key)]);
+        if (object1[(key)] !== object2[(key)])
+        return false;
+      }
+    }
+  }
 
 };
 
@@ -18,8 +28,8 @@ const eqObjects = function(object1, object2) {
 
 // TEST CODE:
 const ab = { a: "1", b: "2"};
-const ba = { b: "1", a: "1"};
+const ba = { b: "2", a: "1"};
 assertEqual(eqObjects(ab, ba), true); // => true
 
-const abc = { a: "1", b: "2", c: "3"};
-assertEqual(eqObjects(ab, abc), false); // => false
+//const abc = { a: "1", b: "2", c: "3"};
+//assertEqual(eqObjects(ab, abc), false); // => false
