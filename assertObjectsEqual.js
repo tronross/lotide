2083dd8ed8eaf888b2cef7ba4a9436ab9b3c6c1e
tronross <john_ross@analogforever.ca>
2022-09-28 16:49:48 -0400

    Copied in and adapted code from assertEqual.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -45,8 +45,14 @@ const eqObjects = function(object1, object2) {
 // assertObjectsEqual: takes in two objects and console.logs an appropriate message to the console.
 
 const assertObjectsEqual = function(actual, expected) {
-
-}
+  const inspect = require('util').inspect;
+  
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
