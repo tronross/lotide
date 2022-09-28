b2da9b300e40a1cc11323be032449da0c06389cc
John Ross <john_ross@analogforever.ca>
2022-09-27 21:59:28 -0400

    Added arrays as values test code.

--- eqObjects.js
+++ eqObjects.js
@@ -31,9 +31,18 @@ return true;
 
 
 // TEST CODE:
+// Primitives:
 const ab = { a: "1", b: "2"};
 const ba = { b: "2", a: "1"};
 assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3"};
 assertEqual(eqObjects(ab, abc), false); // => false
+
+// Arrays as Values:
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] }; 
+assertEqual(eqObjects(cd, cd2), false); // => false
