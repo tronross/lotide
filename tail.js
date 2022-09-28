6e9e83005d6d94b805ec2e90b93551993bc0458e
John Ross <john_ross@analogforever.ca>
2022-09-27 23:12:08 -0400

    Improved test code.

--- tail.js
+++ tail.js
@@ -16,5 +16,5 @@ const tail = function(array) {
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-console.log(tail(words)); // Check tail
-console.log(words); // Check integrity of original array
+console.log("Original array: ", words); // Check integrity of original array
+console.log("Tail: ", tail(words)); // Check tail
