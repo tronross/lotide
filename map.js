26db6364c6cb6f837473dd768ee2d6ee4dfa933d
tronross <john_ross@analogforever.ca>
2022-09-30 15:53:58 -0400

    Pasted in assertion helper code

--- map.js
+++ map.js
@@ -1,3 +1,31 @@
+//ASSERTION HELPER CODE:
+// aasertArraysEqual: a function to compare two arrays and assert if they are equal.
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const arraysEqual = eqArrays(arrayOne, arrayTwo);
+ 
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
+
+// eqArrays: A function to check if arrays are identical.
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+  }
+  return equal;
+};
+
 
 // FUNCTION
 // map: take in an array and a callback function as arguments, and return a new array based on the results of the callback function.
