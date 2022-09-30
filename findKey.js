364bcfa69d51ddde0d5c08badfd9a5754453380b
tronross <john_ross@analogforever.ca>
2022-09-30 19:44:28 -0400

    Initial commit; function declaration and assertion helper test code.

---
+++ findKey.js
@@ -0,0 +1,23 @@
+//ASSERTION HELPER CODE:
+// assertEqual: compare two values and print out a message asserting whether they match or not.
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION
+// findKey: Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
+const findKey = function(object, callback) {
+
+}
+
+
+
+
+
+
+
