1abaed447340e4694398699a3f201b0c19e90358
tronross <john_ross@analogforever.ca>
2022-09-30 15:25:09 -0400

    added for loop to iterate over input array, and temp test code inside the loop to monitor it's effects.

--- map.js
+++ map.js
@@ -1,14 +1,15 @@
 const words = ["a", "screaming", "comes", "across", "the", "sky"];
 
 const map = function(array, callback) {
-  // temp code:
-  console.log('array: ', array);
-  console.log('callback: ', callback);
-  
   const results = [];
+  
+  for (let item of array) {
+  console.log('item BEFORE: ', item);
+  console.log('item AFTER: ', callback(item));
+  console.log('---')
+  }
   return results;
 }
 
 const results1 = map(words, word => word[0]);
-
 console.log(results1);
