2985f884f70f5636a938d52be110e4c707e98366
tronross <john_ross@analogforever.ca>
2022-09-30 16:53:39 -0400

    Initial declaration with arguments; pasted in assertion helper test code.

---
+++ takeUntil.js
@@ -0,0 +1,34 @@
+//ASSERTION HELPER CODE:
+// aasertArraysEqual: a function to compare two arrays and assert if they are equal.
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  const arraysEqual = eqArrays(arrayOne, arrayTwo);
+ 
+  if (arraysEqual) {
+    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
+  }
+};
+
+// eqArrays: A function to check if arrays are identical.
+const eqArrays = function(arrayOne, arrayTwo) {
+  let equal = true;
+
+  if (arrayOne.length !== arrayTwo.length) {
+    equal = false;
+  } else {
+    for (let i = 0; i < arrayOne.length; i++) {
+      if (arrayOne[i] !== arrayTwo[i]) {
+        equal = false;
+      }
+    }
+  }
+  return equal;
+};
+
+
+// FUNCTION
+// takeUntil:
+const takeUntil = function(array, callback) {
+
+}
