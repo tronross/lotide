e2c83ec2a664c5f8855965e9c39a415be1f34d04
tronross <john_ross@analogforever.ca>
2022-09-30 15:08:42 -0400

    Initial commit: very basic testing code and initial function declaration.

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = ["a", "screaming", "comes", "across", "the", "sky"];
+
+const map = function() {
+  const results = [];
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+
+console.log(results1);
