e03cce13292566d2a6460f08f7d4944e172a9de8
tronross <john_ross@analogforever.ca>
2022-09-30 15:29:58 -0400

    Rearranged, added commentary

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
-const words = ["a", "screaming", "comes", "across", "the", "sky"];
-
+// FUNCTION
+// map: take in an array and a callback function as arguments, and return a new array based on the results of the callback function.
 const map = function(array, callback) {
   const results = [];
   
@@ -11,5 +11,6 @@ const map = function(array, callback) {
   return results;
 }
 
+const words = ["a", "screaming", "comes", "across", "the", "sky"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
