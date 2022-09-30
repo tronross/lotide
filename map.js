23e71e6b9dca9eea9d588d2f292d6944956ce3a2
tronross <john_ross@analogforever.ca>
2022-09-30 16:21:17 -0400

    Incorporated assertArraysEqual to test code; linted.

--- map.js
+++ map.js
@@ -42,12 +42,12 @@ const words = ["a", "screaming", "comes", "across", "the", "sky"];
 
 // Case a:
 const results1 = map(words, word => word[0]);
-console.log(results1);
+assertArraysEqual(results1, [ 'a', 's', 'c', 'a', 't', 's' ]);
 
 // Case b:
 const results2 = map(words, word => word.toUpperCase());
-console.log(results2);
+assertArraysEqual(results2, [ 'A', 'SCREAMING', 'COMES', 'ACROSS', 'THE', 'SKY' ]);
 
 // Case c:
 const results3 = map(words, word => word.length);
-console.log(results3);
+assertArraysEqual(results3 , [ 1, 9, 5, 6, 3, 3 ]);
