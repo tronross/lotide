f5787cbec69a826449466b7e64579cbe7967bc20
tronross <john_ross@analogforever.ca>
2022-09-30 20:38:36 -0400

    Lots of test code inside the function to work out the syntax for the callback and iterated array in combination. Identified correct syntax. Works with initial test case.

--- findKey.js
+++ findKey.js
@@ -12,29 +12,30 @@ const assertEqual = function(actual, expected) {
 // FUNCTION
 // findKey: Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
 const findKey = function(object, callback) {
-  let firstKey = "";
 
   for (const item in object) {
-    const key = ""
     console.log(item);
-    console.log(callback(item));
-    //if (item[key] === callback())
+    console.log(object);
+    console.log(callback(object[item]));
+    console.log(object[item]);
+    if (callback(object[item])) {
+      return item;
+    }
   }
-  //return 
 }
 
 
 
 
 
-findKey({
+console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2) // => "noma"
+}, x => x.stars === 2));// => "noma")
 
 // const findKeyByValue = function(objToSearch, searchValue) {
 //   let matchKey = "";
@@ -45,4 +46,4 @@ findKey({
 //     }
 //   }
 //   return matchKey;
-// };
+// }
