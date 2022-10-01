66ee499436d9489e830df6834fdd482f9848ab8f
tronross <john_ross@analogforever.ca>
2022-09-30 21:13:49 -0400

    Linted

--- findKey.js
+++ findKey.js
@@ -22,7 +22,7 @@ const findKey = function(object, callback) {
 
 // TEST CODE
 // Case a:
-const cats = { 
+const cats = {
   Cat1: "Barlo",
   Cat2: "Peach",
   Cat3: "Aktar",
@@ -47,7 +47,7 @@ assertEqual(findKey(lunch, x => x < 10), "Falafel");
 const songs = {
   "The Botttom Line":                 { artist: "O.V. Wright", length: 313 },
   "I Don't Do Windows":               { artist: "O.V. Wright", length: 224 },
-  "That's the Way I Feel About 'Cha": { artist: "O.V. Wright", length: 515 } 
+  "That's the Way I Feel About 'Cha": { artist: "O.V. Wright", length: 515 }
 };
 
 assertEqual(findKey(songs, x => x.length < 300), "I Don't Do Windows");
