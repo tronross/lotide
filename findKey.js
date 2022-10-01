0d35ba4d958311cfe5d35c83fa04068550a86639
tronross <john_ross@analogforever.ca>
2022-09-30 20:15:46 -0400

    Added conditional logic, trying to find syntax to find nested key.

--- findKey.js
+++ findKey.js
@@ -12,22 +12,37 @@ const assertEqual = function(actual, expected) {
 // FUNCTION
 // findKey: Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
 const findKey = function(object, callback) {
+  let firstKey = "";
 
+  for (const item in object) {
+    const key = ""
+    console.log(item);
+    console.log(callback(item));
+    //if (item[key] === callback())
+  }
+  //return 
 }
 
 
 
 
 
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
 
+// const findKeyByValue = function(objToSearch, searchValue) {
+//   let matchKey = "";
 
-const findKeyByValue = function(objToSearch, searchValue) {
-  let matchKey = "";
-
-  for (const key in objToSearch) {
-    if (searchValue === objToSearch[(key)]) {
-      matchKey = key;
-    }
-  }
-  return matchKey;
-};
+//   for (const key in objToSearch) {
+//     if (searchValue === objToSearch[(key)]) {
+//       matchKey = key;
+//     }
+//   }
+//   return matchKey;
+// };
