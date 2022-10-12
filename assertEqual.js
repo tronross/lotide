fddfc68c12e118a6079a32e93c837b1a0bc000b7
tronross <john_ross@analogforever.ca>
2022-10-12 12:56:24 -0400

    Refactored

--- assertEqual.js
+++ assertEqual.js
@@ -8,17 +8,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp"); // Fails
-assertEqual("Barlo is sooo hungry", "Barlo is sooo hungry"); // Passes
-assertEqual(1, 1); // Passes
-assertEqual(75, 956); // Fails
-assertEqual(99, "Ice Cream"); // Fails
-assertEqual(true, true); // Passes
-assertEqual(true, false); // Fails
-console.log("Test for type coercion (string vs number):")
-assertEqual('55', 55); // Fails
-
-
 module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,14 +1,10 @@
 const assertEqual = require('./assertEqual');
 
 // FUNCTION
-// Returns the head of an array as an element.
+// Returns the head of an array.
 const head = function(array) {
   const head = array[0];
   return head;
 };
 
-// Test Cases
-assertEqual(head([5,6,7]), 5); // Passes
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Passes
-assertEqual(head([]), 0); // Fails (empty array)
-assertEqual(head(["So", "much", "meow"]), "So"); // Passes
+module.exports = head;
