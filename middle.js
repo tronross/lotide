597ba47ec9491752d38694ff9423c4313400bfb6
tronross <john_ross@analogforever.ca>
2022-10-12 18:41:16 -0400

    Cleaned, added comments

--- middle.js
+++ middle.js
@@ -3,17 +3,17 @@
 const middle = function(array) {
   let middleArray = [];
   
-  if (array.length <= 2) {
-    return middleArray;
+  if (array.length <= 2) { // return empty array if input array is too short to have a 'middle'
+    return middleArray; 
 
-  } else if ((array.length % 2) !== 0) {
-    const midElement = (Math.floor(array.length / 2));
+  } else if ((array.length % 2) !== 0) { // if odd-numbered .length, return middle element
+    const midElement = (Math.floor(array.length / 2)); 
     middleArray.push(array[midElement]);
-    return middleArray;
+    return middleArray; 
 
-  } else if ((array.length % 2) === 0) {
+  } else if ((array.length % 2) === 0) { // if even-numbered .length, return middle two elements
     const midElelment = (array.length / 2);
-    middleArray.push(array[(midElelment - 1)]);
+    middleArray.push(array[midElelment - 1]);
     middleArray.push(array[midElelment]);
     return middleArray;
   }
