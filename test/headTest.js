f81d5ceefbeaba23138f00d628f43654dd54d49b
tronross <john_ross@analogforever.ca>
2022-10-12 12:54:07 -0400

    Test code for head.js. Linted.

---
+++ test/headTest.js
@@ -0,0 +1,9 @@
+const head = require('../head.js');
+const assertEqual = require('../assertEqual.js');
+
+// TEST CODE
+// Tests successfully returning the head of several arrays, and returning undefined for an empty array.
+assertEqual(head([5,6,7]), 5); // Passes
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Passes
+assertEqual(head(["So", "much", "meow"]), "So"); // Passes
+assertEqual(head([]), 0); // Fails (empty array)
