0f44ece0e2bbbbb9e1a9f1741b43720624ad8370
tronross <john_ross@analogforever.ca>
2022-10-12 16:35:07 -0400

    Improved test code and comments

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,12 +2,20 @@ const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);// => fail
-assertEqual(eqArrays([1, 2, 3], [1, "2", 3]), true);// => fail
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
+console.log("[1, 2, 3], [1, 2, 3] => Pass, identical arrays \n");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // fail
+console.log("[1, 2, 3], [1, 2, 3, 4] => Fail, different .length\n");
+
+assertEqual(eqArrays([1, 2, 3], [1, '2', 3]), true); // fail
+console.log("[1, 2, 3], [1, '2', 3] => Fail, test for type coercion\n");
+
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => fail
-assertEqual(eqArrays(["banana", "canteloupe", "watermelon"], ["banana", "canteloupe", "watermelon"]), true); // => pass
-assertEqual(eqArrays(["banana", "canteloupe", "watermelon"], ["banana", "antelope", "watermelon"]), true); // => fail
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => fail
-assertEqual(eqArrays([true, true],[true, false]), true); // => fail
-assertEqual(eqArrays([true, true],[true, true]), true); // => true
+console.log("[1, 2, 3], [3, 2, 1] => Fail, different element order\n");
+
+assertEqual(eqArrays(['banana', 'canteloupe', 'watermelon'], ['banana', 'canteloupe', 'watermelon']), true); // pass
+console.log("['banana', 'canteloupe', 'watermelon'], ['banana', 'canteloupe', 'watermelon'] => Pass, identical arrays\n");
+
+assertEqual(eqArrays(['banana', 'canteloupe', 'watermelon'], ['banana', 'antelope', 'watermelon']), true); // fail
+console.log("['banana', 'canteloupe', 'watermelon'], ['banana', 'antelope', 'watermelon'] => Fail, different single element\n");
