e4bc12c21eef507e60544c1d947287131091867d
tronross <john_ross@analogforever.ca>
2022-10-12 12:35:09 -0400

    Refactored to require assertEqual.

--- head.js
+++ head.js
@@ -1,21 +1,14 @@
-// Assert Equal testing code
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-// Function to return the head of an array as an element.
+// FUNCTION
+// Returns the head of an array as an element.
 const head = function(array) {
   const head = array[0];
   return head;
 };
 
 // Test Cases
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 0);
-assertEqual(head(["So", "much", "meow"]));
-assertEqual(head(["So", "much", "meow"]), "So");
+assertEqual(head([5,6,7]), 5); // Passes
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Passes
+assertEqual(head([]), 0); // Fails (empty array)
+assertEqual(head(["So", "much", "meow"]), "So"); // Passes
