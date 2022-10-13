9fa0de0187291c00720b2f0db79d027b100a4d4a
tronross <john_ross@analogforever.ca>
2022-10-12 21:25:44 -0400

    Refactored and implemented mocha and chai. Linted.

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,25 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
-// Asserts returning the head of several arrays, and returning undefined for an empty array.
-assertEqual(head([5,6,7]), 5); // Passes
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // Passes
-assertEqual(head(["So", "much", "meow"]), "So"); // Passes
-assertEqual(head([]), 0); // Fails (empty array)
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("returns 'So' for ['So', 'much', 'meow']", () => {
+    assert.strictEqual(head(['So', 'much', 'meow']), 'So');
+  });
+
+  it("fails when trying to return the head of an empty array", () => {
+    assert.isNotOk(head([]), 'this will fail');
+  });
+
+});
