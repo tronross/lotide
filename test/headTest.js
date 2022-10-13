8cdf5384991cc3a71191dabd6302390692a40d28
tronross <john_ross@analogforever.ca>
2022-10-13 12:17:02 -0400

    Rewrote/refactored

--- test/headTest.js
+++ test/headTest.js
@@ -2,11 +2,7 @@ const assert = require('chai').assert;
 const head   = require('../head');
 
 describe("#head", () => {
-  it("returns 1 for [1, 2, 3]", () => {
-    assert.strictEqual(head([1, 2, 3]), 1);
-  });
-
-  it("returns '5' for ['5']", () => {
+  it("returns a single element when fed an array containing a single element", () => {
     assert.strictEqual(head(['5']), '5');
   });
 
