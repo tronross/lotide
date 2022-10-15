4e1617eddfb117b410cba8bbee28b67ec28bd5f5
tronross <john_ross@analogforever.ca>
2022-10-15 13:52:57 -0400

    Implemented require('/...) to clean file

--- without.js
+++ without.js
@@ -1,31 +1,6 @@
-// ASSERTION CODE:
-// A function to compare two arrays and assert if they are equal.
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  const arraysEqual = eqArrays(arrayOne, arrayTwo);
- 
-  if (arraysEqual) {
-    console.log("✅✅✅ Assertion Passed: The arrays are equal.");
-  } else {
-    console.log("🛑🛑🛑 Assertion Failed: The arrays are not equal.");
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
-const eqArrays = function(arrayOne, arrayTwo) {
-  let equal = true; // Set default condition as true
-
-  if (arrayOne.length !== arrayTwo.length) {
-    equal = false; // false if the arrays are different lengths, but also important to ensure accuracy of the following for loop conditionals
-  } else {
-    for (let i = 0; i < arrayOne.length; i++) {
-      if (arrayOne[i] !== arrayTwo[i]) {
-        equal = false; // false if any of the compared elements are not strictly equal
-      }
-    }
-  }
-  return equal;
-};
-
-//FUNCTION
+// FUNCTION
 // without: Takes a source array and an array of elements to remove, and returns a new array that contains the elements _not_ removed. The source array is unchanged.
 const without = function(source, itemsToRemove) {
   let array = source;
@@ -58,3 +33,5 @@ without(words, ["lighthouse"]); // no need to capture return value for this test
 // Make sure the original array was not altered by the without function:
 console.log("Original array: ", OrigWords, "Source array after without(): ", words);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => Pass
+
+module.exports = without;
