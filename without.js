44f9fe39c2aabb91a84b1a14cc49c25bd46c7610
tronross <john_ross@analogforever.ca>
2022-10-15 13:26:50 -0400

    Caught bug: changed function to leave original array unchanged, as per spec. Cleaned, formatted

--- without.js
+++ without.js
@@ -10,31 +10,31 @@ const assertArraysEqual = function(arrayOne, arrayTwo) {
   }
 };
 
-// eqArrays: A function to check if arrays are identical
 const eqArrays = function(arrayOne, arrayTwo) {
-  let equal = true;
+  let equal = true; // Set default condition as true
 
   if (arrayOne.length !== arrayTwo.length) {
-    equal = false;
+    equal = false; // false if the arrays are different lengths, but also important to ensure accuracy of the following for loop conditionals
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
       if (arrayOne[i] !== arrayTwo[i]) {
-        equal = false;
+        equal = false; // false if any of the compared elements are not strictly equal
       }
     }
   }
   return equal;
 };
 
-
+//FUNCTION
 // without: Takes a source array and an array of elements to remove, and returns a new array that contains the elements _not_ removed. The source array is unchanged.
 const without = function(source, itemsToRemove) {
-  let editedArray = source;
+  let array = source;
+  let editedArray = [];
 
   for (let element of itemsToRemove) {
-    for (let i = 0; i < editedArray.length; i++) {
-      if (element === editedArray[i]) {
-        editedArray.splice((i), 1);
+    for (let i = 0; i < array.length; i++) {
+      if (element !== array[i]) {
+        editedArray.push(array[i]);
       }
     }
   }
@@ -42,21 +42,19 @@ const without = function(source, itemsToRemove) {
 };
 
 
-// Test Cases:
+// TEST CODE
 
-console.log(without(["banana", "canteloupe", "watermelon"], ["banana"]));
 assertArraysEqual(without(["banana", "canteloupe", "watermelon"], ["banana"]), ["canteloupe", "watermelon"]); // => Pass
 
-console.log(without(["1", "2", "3"], ["1", "2"]));
-assertArraysEqual(without(["1", "2", "3"], ["1", "2"]), ["3"]); // => Pass
-
-console.log(without([17, 99, 104, 155], [99]));
 assertArraysEqual(without([17, 99, 104, 155], [99]), [17, 104, 155]); // => Pass
 
-
-// Test Case (b): Original array is not changed
+// Test Cases (b): Original array is not changed
 
 const words = ["hello", "world", "lighthouse"];
+const OrigWords = words;
+
 without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => Fail
+
+// Make sure the original array was not altered by the without function:
+console.log("Original array: ", OrigWords, "Source array after without(): ", words);
+assertArraysEqual(words, ["hello", "world", "lighthouse"]); // => Pass
