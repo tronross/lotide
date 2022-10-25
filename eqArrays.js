8c5cdf6ded963ba8434c033471d68af870c59f55
tronross <john_ross@analogforever.ca>
2022-10-25 12:52:32 -0400

    Debug to improve performance

--- eqArrays.js
+++ eqArrays.js
@@ -4,13 +4,13 @@ const eqArrays = function(arrayOne, arrayTwo) {
   let equal = true;  // set default result as true, unless proven false
 
   if (arrayOne.length !== arrayTwo.length) {
-    equal = false; // fail on arrays of different lengths (essential for comparing element by element)
+    return equal = false; // fail on arrays of different lengths (essential for comparing element by element)
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
       if (Array.isArray(arrayOne[i])) { // recursively call itself on nested arrays
         eqArrays(arrayOne[i], arrayTwo[i]);
       } else if (arrayOne[i] !== arrayTwo[i]) { // compare each element in the arrays
-        equal = false;
+        return equal = false;
       }
     }
   }
