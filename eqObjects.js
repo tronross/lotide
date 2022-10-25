a1f55a68ad6bcdeca89f1da87e20e09b396e68ef
tronross <john_ross@analogforever.ca>
2022-10-25 19:04:27 -0400

    refactor to improve performance

--- eqObjects.js
+++ eqObjects.js
@@ -3,27 +3,26 @@ const eqArrays = require('./eqArrays');
 // FUNCTION
 // eqObjects: Takes in two objects and returns true on a perfect match, and false on anything else.
 const eqObjects = function(object1, object2) {
-  let perfectMatch = true;  // set default condition as true
-  const obj1Keys = (Object.keys(object1)).sort(); // create sorted arrays of the input objects keys
+  // set default condition as true
+  let eqObjs = true;
+  
+  // create sorted arrays of the input objects keys
+  const obj1Keys = (Object.keys(object1)).sort();
   const obj2Keys = (Object.keys(object2)).sort();
+  
+  // return false if different number of keys or any of the compared elements (keys) are not strictly equal
+  const keysCheck = eqArrays((obj1Keys), (obj2Keys));
+  if (keysCheck === false) {
+    return eqObjs = false;
+  } 
 
-  if ((Object.keys(object1).length) !== (Object.keys(object2).length)) {
-    perfectMatch = false; // return false on differing number of keys
-  } else {
-    for (const key in object1) {
-      for (const key2 in object2) {
+  for (const key in object1) {
         if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // if property is an array, call eqArray
-          perfectMatch = eqArrays(object1[key], object2[key]);
-        } else if (key === key2) {
-          if (object1[key] !== object2[key]) // return false on differing properties
-            perfectMatch = false;
-        } else {
-          perfectMatch = eqArrays((obj1Keys), (obj2Keys)); // return false if any of the compared elements (keys) are not strictly equal
+          eqObjs = eqArrays(object1[key], object2[key]);
+        } else if (object1[key] !== object2[key]) // return false on differing properties
+          return eqObjs = false;
         }
-      }
-    }
-  }
-  return perfectMatch;
+  return eqObjs;
 };
 
 module.exports = eqObjects;
