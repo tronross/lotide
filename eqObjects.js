bf594f75d85a732e17720451381e60b22f02530d
tronross <john_ross@analogforever.ca>
2022-10-25 19:51:22 -0400

    refactor eqObjects to improve performance

--- eqObjects.js
+++ eqObjects.js
@@ -1,28 +1,35 @@
 const eqArrays = require('./eqArrays');
 
 // FUNCTION
-// eqObjects: Takes in two objects and returns true on a perfect match, and false on anything else.
-const eqObjects = function(object1, object2) {
-  // set default condition as true
-  let eqObjs = true;
-  
-  // create sorted arrays of the input objects keys
-  const obj1Keys = (Object.keys(object1)).sort();
-  const obj2Keys = (Object.keys(object2)).sort();
-  
-  // return false if different number of keys or any of the compared elements (keys) are not strictly equal
+// eqObjects: Takes in two objects and returns true if equal, and false if not equal.
+const eqObjects = function(obj1, obj2) {
+  let eqObjs = true; // set default condition as true
+
+  // create sorted arrays of the input objects' keys;
+  // return false if the keys themselves are not strictly equal
+  const obj1Keys = (Object.keys(obj1)).sort();
+  const obj2Keys = (Object.keys(obj2)).sort();
   const keysCheck = eqArrays((obj1Keys), (obj2Keys));
+  
   if (keysCheck === false) {
     return eqObjs = false;
   }
 
-  for (const key in object1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // if property is an array, call eqArray
-      eqObjs = eqArrays(object1[key], object2[key]);
-    } else if (object1[key] !== object2[key]) // return false on differing properties
+  for (const key in obj1) {
+    
+    // if value is an array, compare it with its corresponding array
+    if (Array.isArray(obj1[key])) {
+      const arraysCheck = eqArrays(obj1[key], obj2[key]);
+
+      if (arraysCheck === false) {
+        return eqObjs = false;
+      }
+
+    } else if (obj1[key] !== obj2[key]) {
       return eqObjs = false;
+    }
   }
-  return eqObjs;
+  return eqObjs; // return true if passed all tests
 };
 
 module.exports = eqObjects;
