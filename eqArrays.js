d0344bfe589ba3004617213928f87461826f5bfc
tronross <john_ross@analogforever.ca>
2022-10-25 18:15:08 -0400

    refactor eqArrays to improve performance

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,16 @@
 // FUNCTION
 // eqArrays: A function to check if arrays are identical
 const eqArrays = function(arrayOne, arrayTwo) {
-  let equal = true;  // set default result as true, unless proven false
+  let equal = true;  // set default return condition as true, unless proven false
 
   if (arrayOne.length !== arrayTwo.length) {
-    return equal = false; // fail on arrays of different lengths (essential for comparing element by element)
+    return equal = false; // immediately fail on arrays of different lengths (essential for comparing element by element)
   } else {
     for (let i = 0; i < arrayOne.length; i++) {
       if (Array.isArray(arrayOne[i])) { // recursively call itself on nested arrays
         eqArrays(arrayOne[i], arrayTwo[i]);
-      } else if (arrayOne[i] !== arrayTwo[i]) { // compare each element in the arrays
-        return equal = false;
+      } else if (arrayOne[i] !== arrayTwo[i]) {
+        return equal = false; // immediately return false if an index does not match between arrays
       }
     }
   }
