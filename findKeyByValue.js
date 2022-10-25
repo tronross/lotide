29660aa7db6925c2c50fb5c7c8965f73f9cd4487
tronross <john_ross@analogforever.ca>
2022-10-25 17:40:19 -0400

    Debug

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -3,14 +3,14 @@ const assertEqual = require('./assertEqual');
 // FUNCTION
 // findKeyByValue: Takes in an object and a value, scans the object and returns the first key which contains the given value. If no key with that value is found, it returns undefined.
 const findKeyByValue = function(objToSearch, searchValue) {
-  let matchKey;
+  let matchKey; // declare default return condition as undefined
 
   for (const key in objToSearch) {
-    if (searchValue === objToSearch[(key)]) {
-      matchKey = key;
+    if (searchValue === objToSearch[key]) {
+      return matchKey = key; // return first matching key
     }
   }
-  return matchKey;
+  return matchKey; // return undefined if no match
 };
 
 // TEST CODE:
@@ -36,5 +36,15 @@ const petNames = {
 assertEqual(findKeyByValue(petNames, "Polar"), "dog");
 assertEqual(findKeyByValue(petNames, "Franz"), "chipmunk");
 assertEqual(findKeyByValue(petNames, "Stevie"), undefined);
+
+// Case 3 (return first key where two keys contain the value):
+const bestTVShowsByGenre2 = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire",
+  test_fi: "The Expanse",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre2, "The Expanse"), "sci_fi");
 
 module.exports = findKeyByValue;
