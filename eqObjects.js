162cd361f6392b5490330757b3f6c47e291c2963
tronross <john_ross@analogforever.ca>
2022-10-25 21:02:38 -0400

    eqObjects: refactor to improve performance and legibility

--- eqObjects.js
+++ eqObjects.js
@@ -2,34 +2,28 @@ const eqArrays = require('./eqArrays');
 
 // FUNCTION
 // eqObjects: Takes in two objects and returns true if equal, and false if not equal.
-const eqObjects = function(obj1, obj2) {
-  let eqObjs = true; // set default condition as true
 
-  // create sorted arrays of the input objects' keys;
-  // return false if the keys themselves are not strictly equal
+const eqObjects = function(obj1, obj2) {
+  // create and compare sorted arrays of the input objects' keys;
   const obj1Keys = (Object.keys(obj1)).sort();
   const obj2Keys = (Object.keys(obj2)).sort();
   const keysCheck = eqArrays((obj1Keys), (obj2Keys));
-  
   if (keysCheck === false) {
-    return eqObjs = false;
+    return false;
   }
 
   for (const key in obj1) {
-    
     // if value is an array, compare it with its corresponding array
     if (Array.isArray(obj1[key])) {
       const arraysCheck = eqArrays(obj1[key], obj2[key]);
-
       if (arraysCheck === false) {
-        return eqObjs = false;
+        return false;
       }
-
     } else if (obj1[key] !== obj2[key]) {
-      return eqObjs = false;
+      return false;
     }
   }
-  return eqObjs; // return true if passed all tests
+  return true; // return true if passes all tests
 };
 
 module.exports = eqObjects;
