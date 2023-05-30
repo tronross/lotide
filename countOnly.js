3d1e46f41f5a6a6c7c344ffdac48332021bc5a83
tronross <john_ross@analogforever.ca>
2023-05-30 17:44:42 -0400

    Format countOnly

--- countOnly.js
+++ countOnly.js
@@ -3,7 +3,7 @@ const assertEqual = require('./assertEqual');
 // FUNCTION
 // countOnly: Takes in a collection of items (strings) and return counts for a specific subset of those items (strings).
 // allItems: an array of strings to be searched, itemsToCount: an object specifying what to count
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function (allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
