688a7b5622c40725fabc2d7660482175600f22bf
tronross <john_ross@analogforever.ca>
2023-05-30 17:43:53 -0400

    Remove test code

--- countOnly.js
+++ countOnly.js
@@ -3,7 +3,7 @@ const assertEqual = require('./assertEqual');
 // FUNCTION
 // countOnly: Takes in a collection of items (strings) and return counts for a specific subset of those items (strings).
 // allItems: an array of strings to be searched, itemsToCount: an object specifying what to count
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
@@ -17,26 +17,5 @@ const countOnly = function (allItems, itemsToCount) {
   }
   return results;
 };
-
-
-// TEST CODE:
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1); // One Jason
-assertEqual(result1["Karima"], undefined); // undefined (no Karimas)
-assertEqual(result1["Fang"], 2); // Two Fangs
-assertEqual(result1["Agouhanna"], undefined); // undefined (no Agouhannas counted (value is false))
 
 module.exports = countOnly;
