822b15696af86946925fe3bf0ee4d8c494048ad9
tronross <john_ross@analogforever.ca>
2023-06-02 11:18:42 -0400

    Format eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -3,12 +3,12 @@ const eqArrays = require('./eqArrays');
 // FUNCTION
 // eqObjects: Takes in two objects and returns true if equal, and false if not equal.
 
-const eqObjects = function(obj1, obj2) {
+const eqObjects = function (obj1, obj2) {
   // create and compare sorted arrays of the input objects' keys;
   const obj1Keys = (Object.keys(obj1)).sort();
   const obj2Keys = (Object.keys(obj2)).sort();
   const keysCheck = eqArrays((obj1Keys), (obj2Keys));
-  
+
   if (keysCheck === false) {
     return false;
   }
@@ -20,7 +20,7 @@ const eqObjects = function(obj1, obj2) {
       if (nestObjCheck === false) {
         return false;
       }
-    // if value is an array, compare it with its corresponding array
+      // if value is an array, compare it with its corresponding array
     } else if (Array.isArray(obj1[key])) {
       const arraysCheck = eqArrays(obj1[key], obj2[key]);
       if (arraysCheck === false) {
