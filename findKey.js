1a64c6563a09a2af8a63f8c46cb9946d6c108939
tronross <john_ross@analogforever.ca>
2023-06-05 11:05:01 -0400

    Format findKey; edit for readability

--- findKey.js
+++ findKey.js
@@ -1,14 +1,12 @@
-const assertEqual = require('./assertEqual');
-
 // FUNCTION
 // findKey: Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
-const findKey = function(object, callback) {
+
+const findKey = function (object, callback) {
   for (const item in object) {
     if (callback(object[item])) {
       return item;
     }
   }
 };
-
 
 module.exports = findKey;
