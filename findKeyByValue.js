cc2dcd6a530e044b46c3f1d9420fd9ac327bbd76
tronross <john_ross@analogforever.ca>
2023-06-07 17:00:03 -0400

    Remove redundant import from findKeyByValue; format; edit for readability

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,7 @@
-const assertEqual = require('./assertEqual');
-
 // FUNCTION
 // findKeyByValue: Takes in an object and a value, scans the object and returns the first key which contains the given value. If no key with that value is found, it returns undefined.
 
-const findKeyByValue = function(objToSearch, searchValue) {
+const findKeyByValue = function (objToSearch, searchValue) {
   for (const key in objToSearch) {
     if (searchValue === objToSearch[key]) {
       return key; // return first matching key
