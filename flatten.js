6e35017b9e26abe480bb26c2f1bff5e112f30f77
tronross <john_ross@analogforever.ca>
2023-06-08 15:49:43 -0400

    Remove imports and test code from flatten

--- flatten.js
+++ flatten.js
@@ -1,5 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
 // FUNCTION
 // flatten: A function which takes in a two-dimensional array and returns a "flattened" version of the array.
 const flatten = function(array) {
@@ -16,21 +14,5 @@ const flatten = function(array) {
   }
   return flattenedArray;
 };
-
-// TEST CODE
-// Test case (a):
-console.log("Source array: ", [1, 2, [3, 4], 5, [6]]);
-console.log("Flattened array: ", flatten([1, 2, [3, 4], 5, [6]]));
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
-
-// Test case (b):
-console.log("Source array: ", ["1", "2", "3", ["11", "12"]]);
-console.log("Flattened array: ", flatten(["1", "2", "3", ["11", "12"]]));
-assertArraysEqual((flatten(["1", "2", "3", ["11", "12"]])), ["1", "2", "3", "11", "12"]);
-
-// Test case (c):
-console.log("Source array: ", ['yes', ['no'], 'maybe']);
-console.log("Flattened array: ", flatten(['yes', ['no'], 'maybe']));
-assertArraysEqual((flatten(['yes', ['no'], 'maybe'])), ['yes', 'no', 'maybe']);
 
 module.exports = flatten;
