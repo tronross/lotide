d33e4a4dd8ecdcd5afbbb3296c545f8069cc597e
tronross <john_ross@analogforever.ca>
2023-06-08 17:16:53 -0400

    Remove imports and test code from letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,12 +1,10 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
 // FUNCTION
 // letterPositions: Returns an object containing keys that correspond to the letters in the input string and arrays containing the indices of where in the string the letters appeared.
 
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
 
-  for (let i = 0; i < sentence.length; i ++) {
+  for (let i = 0; i < sentence.length; i++) {
     let letter = sentence[i];
 
     if (letter !== " ") {
@@ -19,11 +17,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-
-
-// TEST CODE:
-console.log("only hushed murmurs in the villages"); // source string
-console.log(letterPositions("only hushed murmurs in the villages")); // logs the returned object to the console
-assertArraysEqual(letterPositions("only hushed murmurs in the villages").v, [27]); // asserts if the letter v from the input string corresponds to its property in the returned object
 
 module.exports = letterPositions;
