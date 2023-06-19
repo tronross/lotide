a38d76e801f4f9b2ebb96863447d9181f248be9b
tronross <john_ross@analogforever.ca>
2023-06-19 10:51:09 -0400

    Add countOnlyTest.js (chai); copy in v1 test code as skeleton

---
+++ test/countOnlyTest.js
@@ -0,0 +1,22 @@
+const assert = require('chai').assert;
+const flatten = require('../countOnly');
+
+// // TEST CODE:
+// const firstNames = [
+//   "Karl",
+//   "Salima",
+//   "Agouhanna",
+//   "Fang",
+//   "Kavith",
+//   "Jason",
+//   "Salima",
+//   "Fang",
+//   "Joe"
+// ];
+
+// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+// assertEqual(result1["Jason"], 1); // One Jason
+// assertEqual(result1["Karima"], undefined); // undefined (no Karimas)
+// assertEqual(result1["Fang"], 2); // Two Fangs
+// assertEqual(result1["Agouhanna"], undefined); // undefined (no Agouhannas counted (value is false))
