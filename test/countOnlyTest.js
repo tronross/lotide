5ab7b7e3dd0c5aaf99e4f51b99a39ec0b63af6b8
tronross <john_ross@analogforever.ca>
2023-06-26 10:23:42 -0400

    Remove dead code; clean and format

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,7 +1,5 @@
 const assert = require('chai').assert;
-const { expect } = require('chai');
 const countOnly = require('../countOnly');
-const assertEqual = require('../assertEqual');
 
 const firstNames = [
   "Karl",
@@ -43,22 +41,3 @@ describe("#countOnly", () => {
   });
 
 });
-// // TEST CODE:
-// const firstNames = [
-//   "Karl",
-//   "Salima",
-//   "Agouhanna",
-//   "Fang",
-//   "Kavith",
-//   "Jason",
-//   "Salima",
-//   "Fang",
-//   "Joe"
-// ];
-
-// const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1); // One Jason
-// assertEqual(result1["Karima"], undefined); // undefined (no Karimas)
-// assertEqual(result1["Fang"], 2); // Two Fangs
-// assertEqual(result1["Agouhanna"], undefined); // undefined (no Agouhannas counted (value is false))
