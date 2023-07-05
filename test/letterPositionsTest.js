79351b2a289819232b791aba4a742f6101b612b4
tronross <john_ross@analogforever.ca>
2023-07-05 10:32:06 -0400

    Add indices test for letterPositions.

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -6,4 +6,8 @@ describe("#letterPositions", () => {
     assert.deepEqual(letterPositions("only hushed murmurs in the villages").v, [27]);
   });
 
+  it("returns the correct indices of a multiply-occurring letter from an input string", () => {
+    assert.deepEqual(letterPositions("only hushed murmurs in the villages").e, [ 9, 25, 33 ]);
+  });
+
 });
