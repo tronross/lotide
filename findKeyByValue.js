a5799ec5ea4c217e7c31ac9d940f861377ac97dc
tronross <john_ross@analogforever.ca>
2023-07-13 12:19:04 -0400

    Edit comments in findKeyByValue.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,13 +1,22 @@
-// FUNCTION
-// findKeyByValue: Takes in an object and a value, scans the object and returns the first key which contains the given value. If no key with that value is found, it returns undefined.
+///////////////////
+// findKeyByValue
+///////////////////
+
+/**
+ * Takes in an object and a value, scans the object and returns the first key which contains the given value. If no key with that value is found, it returns undefined.
+ *
+ * @param {object} objToSearch Object to scan.
+ * @param {string} searchValue Value to scan for.
+ * @returns {string || undefined} Returns the first matching key as a string, or undefined if no match.
+ */
 
 const findKeyByValue = function (objToSearch, searchValue) {
   for (const key in objToSearch) {
     if (searchValue === objToSearch[key]) {
-      return key; // return first matching key
+      return key;
     }
   }
-  return undefined; // return undefined if no match
+  return undefined;
 };
 
 module.exports = findKeyByValue;
