27ea75e4b809c774eb5c5dd074b9b86ef1b91e4b
tronross <john_ross@analogforever.ca>
2023-07-18 09:42:59 -0400

    Add functional test to countLettersTest (chai)

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -2,6 +2,7 @@ const assert = require('chai').assert;
 const countLetters = require('../countLetters');
 
 const cat = "A cat is not a dog";
+const catCount = { a: 3, c: 1, t: 2, i: 1, s: 1, n: 1, o: 2, d: 1, g: 1 };
 
 describe("#countLetters", () => {
   it("does not mutate the input string", () => {
@@ -9,7 +10,9 @@ describe("#countLetters", () => {
     assert.equal(cat, "A cat is not a dog");
   });
 
- 
-
+  it("correctly counts all the letters in an input string", () => {
+    const countedCat = countLetters(cat);
+    assert.deepEqual(countedCat, catCount);
+  });
 
 });
