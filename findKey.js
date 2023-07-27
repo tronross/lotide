7e8241de8af52a570405a3a10f95450228846c2c
tronross <john_ross@analogforever.ca>
2023-07-27 10:37:24 -0400

    Edit comments in findKey

--- findKey.js
+++ findKey.js
@@ -2,7 +2,13 @@
 // findKey
 ////////////
 
-//: Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
+/**
+ * Takes in an object and a callback, scans the object and returns the first key for which the callback returns a truthy value. (If no key is found, it returns undefined.)
+ *
+ * @param {object} object Object to scan.
+ * @param {function} callback Function to execute on object.
+ * @returns {string || undefined} First truthy key, or undefined.
+ */
 
 const findKey = function (object, callback) {
   for (const item in object) {
