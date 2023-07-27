b41d5fb79ce6ec9cb2abfdfad138d1f783921a7e
tronross <john_ross@analogforever.ca>
2023-07-27 10:28:28 -0400

    Refactor mutation test in countLettersTest

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -6,7 +6,7 @@ const catCount = { a: 3, c: 1, t: 2, i: 1, s: 1, n: 1, o: 2, d: 1, g: 1 };
 
 describe("#countLetters", () => {
   it("does not mutate the input string", () => {
-    const countedCat = countLetters(cat);
+    countLetters(cat);
     assert.equal(cat, "A cat is not a dog");
   });
 
