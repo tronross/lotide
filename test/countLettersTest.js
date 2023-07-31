a442c1764b010473a7372f506ae49c0b8fbf4406
tronross <john_ross@analogforever.ca>
2023-07-31 10:53:43 -0400

    Fix test order in countLettersTest (chai)

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -5,14 +5,14 @@ const cat = "A cat is not a dog";
 const catCount = { a: 3, c: 1, t: 2, i: 1, s: 1, n: 1, o: 2, d: 1, g: 1 };
 
 describe("#countLetters", () => {
-  it("does not mutate the input string", () => {
-    countLetters(cat);
-    assert.equal(cat, "A cat is not a dog");
-  });
-
   it("correctly counts all the letters in an input string", () => {
     const countedCat = countLetters(cat);
     assert.deepEqual(countedCat, catCount);
+  });
+
+  it("does not mutate the input string", () => {
+    countLetters(cat);
+    assert.equal(cat, "A cat is not a dog");
   });
 
 });
