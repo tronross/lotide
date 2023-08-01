436c01f75140793f2a84606b7138aa3bdac62506
tronross <john_ross@analogforever.ca>
2023-08-01 12:22:09 -0400

    Add === (string) callback test to findKeyTest (chai)

--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,2 +1,19 @@
 const assert = require('chai').assert;
 const findKey = require('../findKey');
+
+const cats = {
+  Cat1: "Barlo",
+  Cat2: "Peach",
+  Cat3: "Aktar",
+  Cat4: "Boobooboo",
+  Cat5: "Huxley"
+};
+
+describe("#findKey", () => {
+  it("returns the correct key when callback is strict equal to a string", () => {
+    assert.equal(findKey(cats, x => x === "Peach"), "Cat2");
+  });
+
+
+
+});
