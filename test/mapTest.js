141ebd7217238ec6ffb5c7ca6999dceca93f8245
tronross <john_ross@analogforever.ca>
2023-08-03 14:00:13 -0400

    Add test variable 'words' and initial string test to mapTest (chai)

---
+++ test/mapTest.js
@@ -0,0 +1,14 @@
+const assert = require('chai').assert;
+const map = require('../map');
+
+const words = ["a", "screaming", "comes", "across", "the", "sky"];
+
+describe("#map", () => {
+  it("returns an array containing the first letter of each word when the callback is word => word[0] called on an array of words", () => {
+    const results1 = map(words, word => word[0]);
+    assert.deepEqual(results1, ['a', 's', 'c', 'a', 't', 's']);
+  });
+
+
+
+});
