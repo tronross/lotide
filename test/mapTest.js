b2960e199c34f0c984e955b3602bc31bfca7247c
tronross <john_ross@analogforever.ca>
2023-08-03 14:02:15 -0400

    Edit descriptive text in mapTest (chai)

--- test/mapTest.js
+++ test/mapTest.js
@@ -4,7 +4,7 @@ const map = require('../map');
 const words = ["a", "screaming", "comes", "across", "the", "sky"];
 
 describe("#map", () => {
-  it("returns an array containing the first letter of each word when the callback is word => word[0] called on an array of words", () => {
+  it("returns an array containing the first letter of each string when the callback is word => word[0] called on an array of strings", () => {
     const results1 = map(words, word => word[0]);
     assert.deepEqual(results1, ['a', 's', 'c', 'a', 't', 's']);
   });
