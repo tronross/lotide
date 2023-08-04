eb6a929cb643d6048a52bab679cb95944ae242e6
tronross <john_ross@analogforever.ca>
2023-08-04 09:30:00 -0400

    Edit comments in map

--- map.js
+++ map.js
@@ -6,14 +6,14 @@
  * Takes in an array and a callback function as arguments, and returns a new array based on the results of the callback function.
  *
  * @param {array} array to be processed.
- * @param {function} callback Function to execute on array.
- * @returns {array} New array created by executing callback.
+ * @param {function} callback to execute on array.
+ * @returns {array} created by executing callback.
  */
 
 const map = function (array, callback) {
   const safetyArray = [...array];
   const results = [];
-  
+
   for (let item of safetyArray) {
     results.push(callback(item));
   }
